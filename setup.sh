f2b469d394a149f591ed7e62913e4f31f1ecfbe3
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-01 16:45:49 -0400

    all working except pip install pymysql

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-
-
 # Establish variables for specific versions of libraries
 FLASK_VERSION="${FLASK_VERSION:-0.12.2}"                  #0.12.2
 PEEWEE_VERSION="${PEEWEE_VERSION:-2.10.1}"                #2.10.1
@@ -12,16 +10,6 @@ MYSQLPYTHON_VERSION="${MYSQLPYTHON_VERSION=-1.2.5}"   #1.2.5
 PYDNS="${PYDNS:-2.3.6}"                                   #2.3.6
 
 
-# # Check for virtualenv
-# command -v virtualenv >/dev/null 2>&1 || {
-#     echo >&2 "setup.sh requires 'virtualenv' but it is not installed";
-# }
-#
-# # Check for pip
-# command -v pip3 >/dev/null 2>&1 || {
-#  echo >&2 "source.sh requires 'pip' but it's not installed.";
-# }
-
 # Create the data directory if it doesn't exist
 mkdir -p data
 
@@ -33,45 +21,43 @@ fi
 
 . venv/bin/activate
 
-
-# upgrade pip #Try sudo -H if it doesnt work
-pip3 install --upgrade pip
+# upgrade pip
+pip install --upgrade pip
 
 #install libraries needed for software
 
-pip3 install "flask==$FLASK_VERSION"
+pip install "flask"
 # http://flask.pocoo.org/
 
-pip3 install "peewee==$PEEWEE_VERSION"
+pip install "peewee"
 # http://docs.peewee-orm.com/en/latest/
 
-pip3 install "flask-admin==$FLASK_ADMIN_VERSION"
-# https://flask-admin.readthedocs.io/en/latest/
-
-pip3 install "wtf-peewee==$WTF_PEEWEE_VERSION"
-# https://github.com/coleifer/wtf-peewee
-
-pip3 install "XlsxWriter==$XLSXWRITER_VERSION"
-
-pip3 install "pyyaml==$PYAML_VERSION"
-
-pip3 install "email_validator==$EMAIL_VERSION"
-
-pip3 install "pyDNS==$PYDNS"
-
-pip3 install "MySQL-python" #==$MYSQLPYTHON_VERSION"
-
-pip3 install "pymysql"
-
-pip3 install "flask-admin==$FLASK_ADMIN_VERSION"
-pip3 install "wtf-peewee==$WTF_PEEWEE_VERSION"
-pip3 install "flask_login==$FLASK_LOGIN_VERSION"
-pip3 install git+https://github.com/memo330179/migrant-cli.git
-pip3 install --upgrade setuptools
-pip3 install flask-mysql
-pip3 install --upgrade pip3 enum34
-
-pip3 install git+https://github.com/mzdaniel/loadconfig
-
-pip3 install mysql-connector
- ##FIX ME: ADD UPDATE_SCHEMA PLS ##
+pip install "flask-admin"
+# # https://flask-admin.readthedocs.io/en/latest/
+#
+pip install "wtf-peewee"
+# # https://github.com/coleifer/wtf-peewee
+#
+pip install "XlsxWriter"
+#
+pip install "pyyaml"
+#
+pip install "email_validator"
+#
+pip install "pyDNS"
+#
+pip install "MySQL-python"
+#
+########pip install "pymysql"
+#
+pip install "flask_login"
+pip install git+https://github.com/memo330179/migrant-cli.git
+pip install --upgrade setuptools
+pip install flask-mysql
+pip install --upgrade pip enum34
+#
+pip install git+https://github.com/mzdaniel/loadconfig
+#
+pip install mysql-connector
+#
+#Fix me: ADD UPDATE_SCHEMA 
