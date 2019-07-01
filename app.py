d407aa3b95eda0a2489d91b3d118df1f4746743d
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-01 13:29:20 -0400

    Filled in app.py

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
-app = Flask(__name__)
+'''
+app.py is the starting point of the application; to run the app, in the console, you run "python app.py"
 
-@app.route('/')
-def index():
-    return '<h1>Hello world!</h1>'
+This file should not change often, except maybe to rename the application from "app" to something more meaningful
+such as "helloWorldForm"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+To rename the app, you need to make three changes:
+1) Change  "from app import app" to "from helloWorldForm import app"
+2) Rename the "app" folder to "helloWorldForm"
+3) Rename this file to "helloWorldForm.py"
+
+'''
+import os
+import sys
+
+from app import app
+sys.path.insert(0,'/home/ubuntu/workspace/')
+
+# Builds the server configuration
+if os.getenv('IP'):
+  IP    = os.getenv('IP')
+else:
+  IP    = '0.0.0.0'
+
+if os.getenv('PORT'):
+  PORT  = int(os.getenv('PORT'))
+else:
+  PORT  = 8080
+
+if __name__ == "__main__":
+    # Print statements go to your log file in production; to your console while developing
+    print ("Running server at http://{0}:{1}/".format(IP, PORT))
+    app.run(host = IP, port = PORT, debug = True, threaded = True)
