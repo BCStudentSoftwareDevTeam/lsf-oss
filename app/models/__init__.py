505a08e2c1d8f4a72f39b2f838ea72b629af90a9
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-03 11:00:26 -0400

    added new tables to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,10 @@
 #Import each table in app/models individually
 
 from app.models.laborStatusForm import *
-from app.models.user import *
 from app.models.pastLaborStatusForm import *
+from app.models.laborReleaseForm import *
+from app.models.user import *
 from app.models.student import *
-#from app.models.TABLENAME import *
+from app.models.emailTemplate import *
 #from app.models.TABLENAME import *
 #from app.models.TABLENAME import *
