66139c8e9650d24491a9cf0c21027cfcfc1133cc
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-12 13:46:19 -0400

    added OL form EXCLUDING foreign keys

---
+++ app/models/Tracy/overloadForm.py
@@ -0,0 +1,14 @@
+#TRACY File
+from app.models import *
+
+class overloadForm(baseModel):
+    overloadReason          = CharField(primary_key=True)#Not sure if primary key
+    financialAidApproved    = BooleanField()
+    financialAidApprover    = CharField()#foreign key to USERS
+    financialAidReviewDate  = DateField()
+    SAASApproved            = BooleanField()
+    SAASApprover            = CharField()#foreign key to USERS
+    SAASReviewDate          = DateField()
+    laborApproved           = BooleanField()
+    laborApprover           = CharField()#foreign key to USERS
+    laborReviewDate         = DateField()
