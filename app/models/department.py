d08ae28cfc11109523ef5820d3b28b5dd0330c30
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-12 13:42:12 -0400

    fixed department and added modifiedForm

---
+++ app/models/department.py
@@ -0,0 +1,8 @@
+#lsf File
+from app.models import *
+
+class Department(baseModel):
+    departmentName          = CharField(primary_key=True) #foreign key to lsf
+    departmentAccount       = IntegerField()#I think these are numbers
+    departmentOrg           = IntegerField()#I think these are numbers
+    departmentCompliance    = BooleanField()#True if in compliance, false if out of it
