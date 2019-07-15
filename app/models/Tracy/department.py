b764eeb3bf27e3b99c250636ec5871ee4c60f5a3
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-15 09:51:07 -0400

    got rid of tracy models that were wrong

--- app/models/Tracy/department.py
+++
@@ -1,8 +0,0 @@
-#TRACY File
-from app.models import *
-
-class Department(baseModel):
-    departmentName          = CharField(primary_key=True) #foreign key to lsf
-    departmentAccount       = IntegerField()#I think these are numbers
-    departmentOrg           = IntegerField()#I think these are numbers
-    departmentCompliance    = BooleanField()#True if in compliance, false if out of it
