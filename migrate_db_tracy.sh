bcf38b1812524e33fc8bbd9520b2055da1180414
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-15 09:52:26 -0400

    fixed tracy script

--- migrate_db_tracy.sh
+++ migrate_db_tracy.sh
@@ -5,18 +5,8 @@ pem init
 pem add app.models.Tracy.studata.STUDATA
 pem add app.models.Tracy.stustaff.STUSTAFF
 pem add app.models.Tracy.stuposn.STUPOSN
-pem add app.models.Tracy.term.Term
-pem add app.models.Tracy.user.User
-pem add app.models.Tracy.department.Department
-pem add app.models.Tracy.emailTemplate.EmailTemplate
-pem add app.models.Tracy.formHistory.formHistory
-pem add app.models.Tracy.modifiedForm.modifiedForm
-pem add app.models.Tracy.overloadForm.overloadForm
-pem add app.models.Tracy.status.Status
-pem add app.models.Tracy.student.Student
-pem add app.models.Tracy.laborStatusForm.LaborStatusForm
-pem add app.models.Tracy.laborReleaseForm.LaborReleaseForm
-#what
+
+
 pem watch
 
 pem migrate
