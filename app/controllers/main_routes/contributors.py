30131b9fb042a524b32b7b54a987961a4c60ecee
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-16 16:59:42 -0400

    setting up footer

---
+++ app/controllers/main_routes/contributors.py
@@ -0,0 +1,13 @@
+from app.controllers.main_routes import *
+from app.models.user import *
+from app.models.laborStatusForm import *
+from app.models.term import *
+from flask_bootstrap import bootstrap_find_resource
+from app.models.Tracy.studata import *
+from app import *
+from app.config.loadConfig import *
+
+cfg=get_cfg()
+@app.route("/contributors", methods = ["GET"])
+def contributors():
+    return render_template("main/contributors.html", cfg=cfg)
