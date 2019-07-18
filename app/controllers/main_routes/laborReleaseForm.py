d5a9310ec4666e401178c9c8d5221104fd47fba1
Guillermo Cruz <guillermo_cruz@berea.edu>
2019-07-18 11:19:14 -0400

    Fixed the issue with username being hardcoded in.

--- app/controllers/main_routes/laborReleaseForm.py
+++ app/controllers/main_routes/laborReleaseForm.py
@@ -5,19 +5,18 @@ from app.models.term import *
 from flask_bootstrap import bootstrap_find_resource
 from app.models.Tracy.studata import *
 from app.models.department import *
+from app.models.user import *
 
 @main_bp.route('/laborReleaseForm', methods=['GET', 'POST'])
 # @login_required
 def laborReleaseForm():
 
-    username = load_user('heggens')  #FIXME Hardcoding users is bad
     students = STUDATA.select()
     department = Department.select()
     users = User.select()
 
     return render_template( 'main/laborReleaseForm.html',
 				            title=('Labor Release Form'),
-                            username = username,
                             students = students,
                             department = department,
                             users = users
