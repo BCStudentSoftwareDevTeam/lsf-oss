52171da2a538224ec786d080424e11250e34de9f
Guillermo Cruz <guillermo_cruz@berea.edu>
2019-07-19 08:11:13 -0400

    Fixed the input fields to be required, and fixed the buttons to redirect to the correct pages on click.

--- app/controllers/main_routes/laborReleaseForm.py
+++ app/controllers/main_routes/laborReleaseForm.py
@@ -6,18 +6,29 @@ from flask_bootstrap import bootstrap_find_resource
 from app.models.Tracy.studata import *
 from app.models.department import *
 from app.models.user import *
+from app.controllers.errors_routes.handlers import *
+from app.login_manager import require_login
+
 
 @main_bp.route('/laborReleaseForm', methods=['GET', 'POST'])
 # @login_required
 def laborReleaseForm():
 
+    current_user = require_login()
+    if not current_user:
+        render_template("errors/403.html")
+    if not current_user.isLaborAdmin:
+        render_template("errors/403.html")
+
     students = STUDATA.select()
     department = Department.select()
     users = User.select()
+    laborReleaseform = LaborReleaseForm.select()
 
     return render_template( 'main/laborReleaseForm.html',
 				            title=('Labor Release Form'),
                             students = students,
                             department = department,
-                            users = users
+                            users = users,
+                            laborReleaseForm = laborReleaseForm
                           )
--- app/static/js/laborReleaseForm.js
+++ app/static/js/laborReleaseForm.js
@@ -12,3 +12,18 @@ date.setDate(date.getDate()+1);
 $("#datepicker0").datepicker({
   minDate: date
 });
+$("#datepicker0").datepicker("setDate", "date");
+
+$(function() {
+   $("#submit").click(function() {
+    if($("#condition").val() == "" || $("#datepicker0").val() == "" || $("#reason").val() == "") {
+      category = "danger"
+      msg = "Please fill out all fields before submitting.";
+      $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>')
+      $("#flasher").delay(3000).fadeOut()
+    }
+    else {
+      $("#modal").modal("show");
+    }
+   });
+});
