5d8db5c0a55b846c73a5cc1a12ea765a3508204b
sheggen <heggens@berea.edu>
2019-07-19 09:02:25 -0400

    Giving you a new state to work from

--- app/static/js/manageDepartments.js
+++ app/static/js/manageDepartments.js
@@ -16,14 +16,9 @@
 
 
 $(document).ready( function(){
-x = $('#departmentsTable')
-//console.log(x);
-x.DataTable( {
-  // "columnDefs": [ {
-  //     "type": "compliance-status",
-  //     "targets": 1
-  // } ]
-});
+    x = $('#departmentsTable');
+    //console.log(x);
+    x.DataTable();
 });
 
 function status(department) {
@@ -42,21 +37,22 @@ function status(department) {
         msg = "Department compliance changed.";
         $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>')
         $("#flasher").delay(3000).fadeOut()
-        var departmentID = $("#" + department);
+        var departmentID = $("#dept_btn_" + department);
         //console.log(departmentID);
         if ($(departmentID).hasClass("btn-success")){
           $(departmentID).removeClass("btn-success");
           $(departmentID).addClass("btn-danger");
           $(departmentID).text("Not in Compliance");
-          //$(departmentID).attr("data-order", -1);
+          $("#dept_" + department).attr("data-order", -1);
         }
         else {
           $(departmentID).removeClass("btn-danger");
           $(departmentID).addClass("btn-success");
           $(departmentID).text("In Compliance");
-          //$(departmentID).attr("data-order", 1);
+          $("#dept_" + department).attr("data-order", 1);
         }
+        $('#departmentsTable').DataTable().ajax.reload();
+      }
     }
-  }
-    })
-  };
+  })
+};
