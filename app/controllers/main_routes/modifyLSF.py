adce78799c99c1047e8f2a94fc4f978ed0f8a71d
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-22 14:41:31 -0400

    pushing local

--- app/controllers/main_routes/modifyLSF.py
+++ app/controllers/main_routes/modifyLSF.py
@@ -1,7 +1,11 @@
 from app.controllers.main_routes import *
+from app.controllers.main_routes.main_routes import *
+from app.controllers.main_routes.laborHistory import *
 from app.models.user import *
+from app.models.Tracy.studata import STUDATA
 from flask_bootstrap import bootstrap_find_resource
 from app.login_manager import require_login
+
 
 
 @main_bp.route('/modifyLSF', methods=['GET', 'POST'])
@@ -10,18 +14,33 @@ def modifyLSF():
     current_user = require_login()
     if not current_user:        # Not logged in
         return render_template('errors/403.html')
-    return render_template( 'main/modifyLSF.html',
-				            title=('Modify LSF'),
-                            username = current_user
-                          )
-@main_bp.route('/modifyLSF/getPrefills', methods=['GET'])
-def getPrefills():
-    #Function that pulls info/data from form history that user clicked to populate modify forms
-    #Step 1: get form attached to the student
-    # thestudent = LaborStatusForm.select(LaborStatusForm.studentSupervisee).where(LaborStatusForm.)#query to find student associated with form
-    # theform = #query to find specific form they clicked from (from all student's forms)
-    #Step 2: get data from said forms
+    #If logged in....
+    #Step 1: get form attached to the student (via labor history modal)
+
+    #Step 2: get data from said form
+    prefillstudent = 1
+    prefillsupervisor = 2
+    prefilldepartment = 3
+    prefillposition = 4
+    prefillwls = 5
+    prefilljobtype = 6
+    prefillterm = 7
+    prefillhours = 8
+    prefilldateneeded = 9
+    prefillnotes = 10
 
     #Step 3: send data to front to populate html
-
-    return render_template( '/modifyLSF/getPrefills',)
+    return render_template( 'main/modifyLSF.html',
+				            title=('Modify LSF'),
+                            username = current_user,
+                            prefillstudent = prefillstudent,
+                            prefillsupervisor = prefillsupervisor,
+                            prefilldepartment = prefilldepartment,
+                            prefillposition = prefillposition,
+                            prefillwls = prefillwls,
+                            prefilljobtype = prefilljobtype,
+                            prefillterm = prefillterm,
+                            prefillhours = prefillhours,
+                            prefilldateneeded = prefilldateneeded,
+                            prefillnotes = prefillnotes
+                          )
