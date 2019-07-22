7de20efff57cb52e0990c13f460b77e36f43309b
Roberto Santos Hernandez <santoshernandezr@berea.edu>
2019-07-22 13:49:11 -0400

    Worked on the styling of the modal buttons and boxes

--- app/static/js/adminManagement.js
+++ app/static/js/adminManagement.js
@@ -1,31 +1,37 @@
 $(document).ready(function() { console.log("Howdy") });
 function modal(button) {
   if(button == "add" && $("#addlaborAdmin").val() != "") {
+    $("p").html("Are you sure you want to add " + $("#addlaborAdmin").val() + " as an admin?");
     document.getElementById("submitModal").setAttribute("name", "add");
     document.getElementById("submitModal").setAttribute("value", "add");
     $("#modal").modal("show");
   }
   else if(button == "add1" && $("#addFinAidAdmin").val() != "") {
+    $("p").html("Are you sure you want to add " + $("#addFinAidAdmin").val() + " as an admin?");
     document.getElementById("submitModal").setAttribute("name", "addAid");
     document.getElementById("submitModal").setAttribute("value", "addAid");
     $("#modal").modal("show");
   }
   else if(button == "add2" && $("#addSaasAdmin").val() != "") {
+    $("p").html("Are you sure you want to add " + $("#addSaasAdmin").val() + " as an admin?");
     document.getElementById("submitModal").setAttribute("name", "addSaas");
     document.getElementById("submitModal").setAttribute("value", "addSaas");
     $("#modal").modal("show");
   }
   else if(button == "remove" && $("#removelaborAdmin").val() != "") {
+    $("p").html("Are you sure you want to remove " + $("#removelaborAdmin").val() + "?");
     document.getElementById("submitModal").setAttribute("name", "remove");
     document.getElementById("submitModal").setAttribute("value", "remove");
     $("#modal").modal("show");
   }
   else if(button == "remove1" && $("#removeFinAidAdmin").val() != "") {
+    $("p").html("Are you sure you want to remove " + $("#removeFinAidAdmin").val() + "?");
     document.getElementById("submitModal").setAttribute("name", "removeAid");
     document.getElementById("submitModal").setAttribute("value", "removeAid");
     $("#modal").modal("show");
   }
   else if(button == "remove2" && $("#removesSaasAdmin").val() != "") {
+    $("p").html("Are you sure you want to remove " + $("#removeSaasAdmin").val() + "?");
     document.getElementById("submitModal").setAttribute("name", "removeSaas");
     document.getElementById("submitModal").setAttribute("value", "removeSaas");
     $("#modal").modal("show");
@@ -37,20 +43,3 @@ function modal(button) {
     $("#flasher").delay(3000).fadeOut()
 }
 };
-
-
-
-// $(function() {
-//    $("#add").click(function() {
-//     if($("#addlaborAdmin").val() == "") {
-//       console.log($('#addlaborAdmin').val())
-//       category = "danger"
-//       msg = "Please select a user";
-//       $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>')
-//       $("#flasher").delay(3000).fadeOut()
-//     }
-//     else {
-//       $("#modal").modal("show");
-//     }
-//    });
-//   });
