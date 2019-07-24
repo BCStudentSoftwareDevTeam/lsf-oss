bb3665db040b1b30bf76a796c620b79cbc388b51
Guillermo Cruz <guillermo_cruz@berea.edu>
2019-07-24 13:29:50 -0400

    Stashing away some code for now.

--- app/controllers/main_routes/laborHistory.py
+++ app/controllers/main_routes/laborHistory.py
@@ -7,6 +7,8 @@ from app.models.formHistory import *
 from app.models.student import *
 from app.controllers.errors_routes.handlers import *
 from app.login_manager import require_login
+from flask import json
+from flask import make_response
 
 @main_bp.route('/laborHistory/<id>', methods=['GET', 'POST'])
 # @login_required
@@ -20,38 +22,11 @@ def laborhistory(id):
         student = Student.get(Student.ID == id)
         # print(student)
         studentForms = LaborStatusForm.select().where(LaborStatusForm.studentSupervisee == student)
-        # print("Here")
-        # try:
-        #     for k in studentForms:
-        #         print(k)
-        # except Exception as e:
-        #     print(e)
-        # forms = FormHistory.select()
-        # try:
-        #     for i in forms:
-        #         print(i)
-        # except Exception as e:
-        #     print(e)
-        # print("Down here")
-        # print(studentForms)
-        # try:
-        #     for i in studentForms:
-        #         print(i.formID.studentSupervisee.FIRST_NAME)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(studentForms.formID.studentSupervisee.FIRST_NAME)
-        # except Exception as e:
-        #     print(e)
-
-        #for form in studentForms:
-            #print(form)
         return render_template( 'main/laborhistory.html',
     				            title=('Labor History'),
                                 student = student,
                                 username=current_user.username,
                                 studentForms = studentForms,
-                                #forms = forms
                               )
     except:
         render_template('errors/500.html')
@@ -59,17 +34,40 @@ def laborhistory(id):
 @main_bp.route('/laborHistory/modal/<statusKey>', methods=['GET'])
 
 def populateModal(statusKey):
+    # try:
+    #     forms = FormHistory.select().where(FormHistory.formID == statusKey)
+    #     print(statusKey)
+    #     for k in forms:
+    #         print(k.modifiedForm)
+    #     modalData = {}
+    #     for i in forms:
+    #         modalData.update({"createdDate": i.createdDate,
+    #                         "createdBy": i.createdBy.FIRST_NAME + " " + i.createdBy.LAST_NAME,
+    #                         "jobType": i.formID.jobType,
+    #                         "WLS": i.formID.WLS,
+    #                         "contractHours": i.formID.contractHours,
+    #                         "weeklyHours": i.formID.weeklyHours,
+    #                         "supervisorNotes": i.formID.supervisorNotes,
+    #                         "term": i.formID.termCode.termName,
+    #                         "positionTitle": i.formID.POSN_TITLE,
+    #                         "positionCode": i.formID.POSN_CODE,
+    #                         "studentName": i.formID.studentSupervisee.FIRST_NAME + " " + i.formID.studentSupervisee.LAST_NAME
+    #                         })
+    #     return json.dumps(modalData)
+    # except Exception as e:
+    #     print(e)
+    #     return (jsonify({"Success": False}))
     try:
         forms = FormHistory.select().where(FormHistory.formID == statusKey)
-        modalData = {}
-        for i in forms:
-            print(i.formID.WLS)
-            print(i.formID.studentSupervisee.FIRST_NAME)
-            modalData[statusKey] = {"createdDate": i.rejectReason}
+        statusForm = LaborStatusForm.select().where(LaborStatusForm.laborStatusFormID == statusKey)
+        print("here")
         print(statusKey)
-        print(modalData)
-        modalData[statusKey] = {""}
-        return (jsonify({"Success": True}))
+        resp = make_response(render_template('snips/studentHistoryModal.html',
+                                            forms = forms,
+                                            statusForm = statusForm
+                                            ))
+        return (resp)
     except Exception as e:
         print(e)
+        render_template('errors/500.html')
         return (jsonify({"Success": False}))
--- app/static/js/laborhistory.js
+++ app/static/js/laborhistory.js
@@ -1,16 +1,32 @@
 function hello(laborStatusKey) {
-  console.log(laborStatusKey)
 
   $.ajax({
     type: "GET",
     url: '/laborHistory/modal/' + laborStatusKey,
     //data: JSON.stringify({"laborStatusFormID": laborStatusKey}),
-    success: function(request) {
+    success: function(response) {
+      $("#holdModal").append(response);
       $("#modal").modal("show");
     }
   });
 
-
-  //$("#modal").modal("show");
-
 }
+
+
+
+// function hello(laborStatusKey) {
+//
+//   $.ajax({
+//     type: "GET",
+//     url: '/laborHistory/modal/' + laborStatusKey,
+//     //data: JSON.stringify({"laborStatusFormID": laborStatusKey}),
+//     success: function(modalDictionary) {
+//       var modalData = JSON.parse(modalDictionary)
+//       console.log(modalData);
+//       $("p").html(modalData["studentName"])
+//       $("#holdModal").append(response)
+//       $("#modal").modal("show");
+//     }
+//   });
+//
+// }
