81fcf4c2d7ee0a524a83d5de2e976371731785a2
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-07-25 09:43:31 -0400

    Primary supervisor is now just supervisor, and no secondary

--- app/models/laborStatusForm.py
+++ app/models/laborStatusForm.py
@@ -10,9 +10,9 @@ class LaborStatusForm (baseModel):
     laborStatusFormID           = IntegerField(primary_key=True)
     termCode                    = ForeignKeyField(Term, on_delete="cascade")             # FK to term
     studentSupervisee           = ForeignKeyField(Student, on_delete="cascade")          # foreign key to student
-    primarySupervisor           = ForeignKeyField(User, on_delete="cascade")             # foreign key to user
+    supervisor                  = ForeignKeyField(User, on_delete="cascade")             # foreign key to user
     department                  = ForeignKeyField(Department, on_delete="cascade")       # Foreign key to department
-    secondarySupervisor         = ForeignKeyField(User, null=True, on_delete="cascade")  # student may not always have a secondary
+    #secondarySupervisor         = ForeignKeyField(User, null=True, on_delete="cascade")  # student may not always have a secondary #7/25/19: decided it was too                                                                                                 confusing language and we didnt even need to store this field..
     jobType                     = CharField()                       # Primary or secondary
     WLS                         = CharField()                       
     POSN_TITLE                  = CharField()                       # eg. student programmer, customer engagement specialist, receptionist, teaching assistant
