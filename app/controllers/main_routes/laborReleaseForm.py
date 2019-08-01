11e2dac3556656f6c7fa21c00054c49b8e5a8270
Guillermo Cruz <guillermo_cruz@berea.edu>
2019-08-01 13:18:06 -0400

    Made the page redirct, and made the flash message appear on the redirct.

--- app/controllers/main_routes/laborReleaseForm.py
+++ app/controllers/main_routes/laborReleaseForm.py
@@ -9,12 +9,13 @@ from app.models.department import *
 from app.models.user import *
 from app.controllers.errors_routes.handlers import *
 from app.login_manager import require_login
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, flash
 from flask import request
-from flask import jsonify
 
+# @main_bp.route('/laborReleaseForm/<laborStatusKey>', methods=['GET', 'POST'])
 @main_bp.route('/laborReleaseForm', methods=['GET', 'POST'])
 # @login_required
+
 def laborReleaseForm():
 
     current_user = require_login()
@@ -26,10 +27,32 @@ def laborReleaseForm():
     students = Student.select()
     department = Department.select()
     users = User.select()
+    # forms = LaborStatusForm.select().distinct().where(LaborStatusForm.laborStatusFormID == laborStatusKey)
     forms = LaborStatusForm.select().distinct().where(LaborStatusForm.laborStatusFormID == 21) #FIXEME: This ID needs to come from the modal from the supervisor portal
     #forms = FormHistory.select().distinct().where(FormHistory.formHistoryID == 1)
 
-    return render_template( 'main/laborReleaseForm.html',
+    if(request.method == 'POST'):
+        try:
+            date = request.form.get("date")
+            print (date)
+            reason = request.form.get("notes")
+            print (reason)
+            condition = request.form.get("condition")
+            print (condition)
+            for i in forms:
+                print(i.laborStatusFormID)
+
+
+
+
+            flash("You labor release form has been submitted.")
+            return redirect(url_for("main.index"))
+
+        except Exception as e:
+            print(e)
+            return jsonify({"Success": False})
+
+    return render_template('main/laborReleaseForm.html',
 				            title=('Labor Release Form'),
                             students = students,
                             department = department,
@@ -37,20 +60,22 @@ def laborReleaseForm():
                             forms = forms
                           )
 
-@main_bp.route("/index/second", methods=['POST'])
-
-def createReleaseForm():
-    try:
-        print("Made it into the try")
-        date = request.form.get("date")
-        print (date)
-        reason = request.form.get("notes")
-        print (reason)
-        condition = request.form.get("condition")
-        print (condition)
-        #rsp = eval(request.data.decode("utf-8"))
-        return redirect(url_for("main.index"))
-
-    except Exception as e:
-        print(e)
-        return jsonify({"Success": False})
+# @main_bp.route("/index/second", methods=['POST'])
+#
+# def createReleaseForm():
+#     try:
+#         date = request.form.get("date")
+#         print (date)
+#         reason = request.form.get("notes")
+#         print (reason)
+#         condition = request.form.get("condition")
+#         print (condition)
+#         laborStatusKey = request.form.get("student")
+#         print (laborStatusKey)
+#         #rsp = eval(request.data.decode("utf-8"))
+#         flash("Hello World!", "error")
+#         return redirect(url_for("main.index"))
+#
+#     except Exception as e:
+#         print(e)
+#         return jsonify({"Success": False})
