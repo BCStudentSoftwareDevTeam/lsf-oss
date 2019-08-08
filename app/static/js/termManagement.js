b30c74232899263e1e4f284e6e27d5e63ea7ca7c
Roberto Santos Hernandez <santoshernandezr@berea.edu>
2019-08-08 17:05:34 -0400

    fixed some stuff

--- app/static/js/termManagement.js
+++ app/static/js/termManagement.js
@@ -13,6 +13,62 @@ date.setDate(date.getDate());
 $(".form-control").datepicker({
 });
 
+$(document).ready(function () {
+  $(".start").datepicker({
+      dateFormat: "M-dd-yy",
+      minDate: 0,
+      onSelect: function (date) {
+          var date2 = $('.end').datepicker('getDate');
+          date2.setDate(date2.getDate() + 1);
+          $('.start').datepicker('setDate', date2);
+          //sets minDate to dt1 date + 1
+          $('.start').datepicker('option', 'minDate', date2);
+      }
+  });
+  $('.start').datepicker({
+      dateFormat: "M-dd-yy",
+      onClose: function () {
+          var dt1 = $('.start').datepicker('getDate');
+          console.log(dt1);
+          var dt2 = $('.end').datepicker('getDate');
+          if (dt2 <= dt1) {
+              var minDate = $('#dt2').datepicker('option', 'minDate');
+              $('.end').datepicker('setDate', minDate);
+          }
+      }
+  });
+});
+
+// function DateCheck()
+// {
+//   var StartDate= document.getElementById('start_201700').value;
+//   var EndDate= document.getElementById('end_201700').value;
+//   var eDate = new Date(EndDate);
+//   var sDate = new Date(StartDate);
+//   if(StartDate!= '' && StartDate!= '' && sDate> eDate)
+//     {
+//     alert("Please ensure that the End Date is greater than or equal to the Start Date.");
+//     return false;
+//     }
+// }
+
+// function {
+//   $("#start_201700").datepicker({
+//       onClose: function(selected) {
+//         var dt = new Date(selected);
+//         dt.setDate(dt.getDate() + 1);
+//         $("#end_201700").datepicker("option","minDate", dt);
+//       }
+//   });
+//   // $("#end_201700").datepicker({
+  //     onClose: function(selected) {
+  //       var dt = new Date(selected);
+  //       dt.setDate(dt.getDate() - 1);
+  //       $("#start_201700").datepicker("option","maxDate", dt);
+  //     }
+  // });
+// }
+
 function getDate(obj, termCode) {
   var termStart = obj.value; // This is the start date
   var termID = obj.id.split("_")[1] //
@@ -34,7 +90,6 @@ function getDate(obj, termCode) {
         console.log(response)
         if(response)
         stateBtnValue = $("#term_btn_" + termCode).val();
-        console.log("Here is the state button Value " + stateBtnValue)
         start = $("#start_" + termCode).val();
         console.log(start)
         end = $("#end_" +termCode).val();
@@ -43,6 +98,13 @@ function getDate(obj, termCode) {
           $('#term_btn_' + termCode).prop('disabled', false)
           console.log("It worked!!")
         }
+    //     $("#start_" + termCode).datepicker({
+    //     onClose: function(selected) {
+    //       console.log($("#end_" + termCode), $("#start_" + termCode));
+    //       console.log("hello")
+    //         $("#end_" + termCode).datepicker("option", "minDate", selected);
+    //     }
+    // });
         console.log("js success")
       }
     });
