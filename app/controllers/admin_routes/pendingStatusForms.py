29784a00e9e25aae026f6b6cdc8f1c91adca4592
BriaW <briacodes@gmail.com>
2019-08-13 17:06:06 -0400

    Created a table with a pending status form. Woohoo!

--- app/controllers/admin_routes/pendingStatusForms.py
+++ app/controllers/admin_routes/pendingStatusForms.py
@@ -4,6 +4,10 @@ from flask import flash, send_file
 from app.login_manager import *
 from app.controllers.admin_routes import admin
 from app.controllers.errors_routes.handlers import *
+from app.models.laborReleaseForm import LaborReleaseForm
+from app.models.laborStatusForm import LaborStatusForm
+from app.models.modifiedForm import ModifiedForm
+from app.models.overloadForm import OverloadForm
 from app.models.formHistory import *
 from app.models.term import Term
 from datetime import datetime, date
@@ -18,29 +22,44 @@ def pendingForms():
             return render_template('errors/403.html')
 
 
-        pending_status_forms = FormHistory.select().where(FormHistory.status == "Pending").order_by(-FormHistory.createdDate)
-        print(len(pending_status_forms))
-        # Logged in & Admin
+        # pending_status_forms = FormHistory.select().where(FormHistory.status == "Pending").order_by(-FormHistory.createdDate)
+        pending_labor_forms = FormHistory.select().where(FormHistory.status == "Pending").where(FormHistory.historyType == "Labor Status Form").order_by(-FormHistory.createdDate)
+        # pending_release_forms = FormHistory.select().where(FormHistory.status == "Pending").where(FormHistory.historyType == "Labor Release Form").order_by(-FormHistory.createdDate)
+        # pending_overload_forms = FormHistory.select().where(FormHistory.status == "Pending").where(FormHistory.historyType == "Labor Overload Form").order_by(-FormHistory.createdDate)
+        # pending_modified_forms = FormHistory.select().where(FormHistory.status == "Pending").where(FormHistory.historyType == "Modified Labor Form").order_by(-FormHistory.createdDate)
+        #
+        # pendingModifiedList = []
+        # for i in pending_labor_forms:
+        #     print(str(i) + " this should be 1")
+        #
+        # for i in pending_release_forms:
+        #     print(str(i) + " this should be 7")
+        #
+        # for i in pending_overload_forms:
+        #     print(str(i) + "this should be 11")
+        #
+        # for i in pending_modified_forms:
+        #     pendingModifiedList.append(i)
+        #     print(pendingModifiedList)
+        #
+        #
+        #
+        # #print(len(pending_status_forms))
+        # # Logged in & Admin
         users = User.select()
+
+
         return render_template( 'admin/pendingStatusForms.html',
                                 title=('Pending Forms'),
                                 username=current_user.username,
                                 users=users,
-                                pending_status_forms = pending_status_forms
+                                pending_labor_forms = pending_labor_forms
+                                # pending_status_forms = pending_status_forms
+                                # pending_modified_forms = pending_modified_forms,
+                                # pending_release_forms = pending_release_forms,
+                                # pending_overload_forms = pending_overload_forms
                                 )
-    except:
-        render_template('errors/500.html')
-# def index():
-#     current_user = require_login()
-#     #print(current_user)
-#     if not current_user:
-#         return render_template('errors/403.html')
-#
-#    pending_status_forms =  formHistory.select().where(LaborStatusForm.supervisor == current_user.username).order_by(LaborStatusForm.endDate.desc())
-#     # departmentID = Department.get(Department.DEPT_NAME == current_user.DEPT_NAME)
-#     # departmentStudents = LaborStatusForm.select().join(Term).where(LaborStatusForm.termCode.termEnd >= todayDate).where(LaborStatusForm.department == departmentID)
-#
-#     # pastDepartmentStudents = LaborStatusForm.select().join(Term).where(LaborStatusForm.termCode.termEnd >= todayDate).where(LaborStatusForm.department == departmentID)
-#     currentDepartmentStudents = LaborStatusForm.select().join_from(LaborStatusForm, Term).join_from(LaborStatusForm, Department).where(LaborStatusForm.termCode.termEnd >= todayDate).where(LaborStatusForm.department.DEPT_NAME == current_user.DEPT_NAME)
-#
-#     pastDepartmentStudents = LaborStatusForm.select().join_from(LaborStatusForm, Department).where(LaborStatusForm.department.DEPT_NAME == current_user.DEPT_NAME)
+
+    except Exception as e:
+        print(e)
+        return render_template('errors/500.html')
