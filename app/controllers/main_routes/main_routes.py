48a6b5620ef7cc486cd6b22e53cdee88eae9133e
Guillermo Cruz <guillermo_cruz@berea.edu>
2019-08-23 13:25:59 -0400

    Made some changes to dummy data

--- app/controllers/main_routes/main_routes.py
+++ app/controllers/main_routes/main_routes.py
@@ -29,7 +29,10 @@ def index():
     todayDate = date.today()
     # Grabs all the labor status forms where the current user is the supervisor
     formsBySupervisees = LaborStatusForm.select().where(LaborStatusForm.supervisor == current_user.username).order_by(LaborStatusForm.endDate.desc())
-    allCurrentUserForms = FormHistory.select(FormHistory.formID.department).join_from(FormHistory, LaborStatusForm).where((FormHistory.createdBy == current_user.username) or (FormHistory.formID.supervisor == current_user.username)).distinct()
+    # allCurrentUserForms = FormHistory.select(FormHistory.formID.department).join_from(FormHistory, LaborStatusForm).where((FormHistory.createdBy == current_user.username) or (FormHistory.formID.supervisor == current_user.username)).distinct()
+
+
+    allCurrentUserForms = FormHistory.select().where(FormHistory.createdBy == current_user.username)
     # Grabs all the labor status forms where the current users department matches the status forms derpartment, and where the current date is less than the term end date on the status form
     currentDepartmentStudents = LaborStatusForm.select().join_from(LaborStatusForm, Department).where(LaborStatusForm.endDate >= todayDate).where(LaborStatusForm.department.DEPT_NAME == "Computer Science")
     # Grabs all the labor status forms where the current users department matches the status forms derpartment
@@ -37,8 +40,7 @@ def index():
     print(current_user.DEPT_NAME)
     ##### Testing Code
     for i in allCurrentUserForms:
-        print(i.formID)
-    print(allCurrentUserForms)
+        print(i)
     #####
 
     inactiveSupervisees = []
