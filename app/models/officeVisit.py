92a6ffd0ca0bb42c1d233eae963e21e2d2114a40
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-10-30 16:33:30 -0400

    added isStudent flag, removed Other field

--- app/models/officeVisit.py
+++ app/models/officeVisit.py
@@ -6,11 +6,12 @@ from app.models.student import Student
 # All caps fields are pulled from TRACY
 class officeVisit (baseModel):
     officeVisitID       = PrimaryKeyField()
-    ID   				= CharField(null=True)# B-number
-	FIRST_NAME  		= CharField(null=True)
-	LAST_NAME  			= CharField(null=True)
-    reason              = CharField(null=True) #Paycheck, Direct deposit, etc, Other. If Other, "other" field will no longer be null and the reason eneterd by the user will be stored here.
-    other               = CharField(null=True) #If the reason is other, this field will hold the reason
+    ID   			        	= CharField(null=True)# B-number
+	  FIRST_NAME  	    	= CharField(null=True)
+	  LAST_NAME  			    = CharField(null=True)
+    reason              = CharField(null=True) #Paycheck, Direct deposit, etc, Other. If Other, store the "other" reason the user entered here
+    isStudent           = BooleanFields(null=True) #If someone has a b#, this flag should be set to True. 
+    #There are cases in which staff comes in, though, and they DO have B#s. Ahad and Kat said they could indicate this rare case in the comments if its really necessary, but this flag is moreso for tracking non-BC affiliated visits.
     comments            = CharField(null=True) #additional comment box below reason
 
 
