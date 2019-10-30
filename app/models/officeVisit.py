eaffea956c8476b6d3f478bfee106c3eae03402c
Kat Adams & Bria Williams <paesh1997@bitbucket.org>
2019-10-30 16:37:11 -0400

    whitespace

--- app/models/officeVisit.py
+++ app/models/officeVisit.py
@@ -6,9 +6,9 @@ from app.models.student import Student
 # All caps fields are pulled from TRACY
 class officeVisit (baseModel):
     officeVisitID       = PrimaryKeyField()
-    ID   			    = CharField(null=True)# B-number
-	FIRST_NAME  	    = CharField(null=True)
-	LAST_NAME  	        = CharField(null=True)
+    ID   			          = CharField(null=True)# B-number
+	  FIRST_NAME  	      = CharField(null=True)
+	  LAST_NAME  	        = CharField(null=True)
     reason              = CharField(null=True) #Paycheck, Direct deposit, etc, Other. If Other, store the "other" reason the user entered here
     isStudent           = BooleanFields(null=True) #If someone has a b#, this flag should be set to True.
     #There are cases in which staff comes in, though, and they DO have B#s. Ahad and Kat said they could indicate this rare case in the comments if its really necessary, but this flag is moreso for tracking non-BC affiliated visits.
