73ec66dbf5e82d6018f507b12285893c90dfd7a2
Luis Riera <rieral@berea.edu>
2019-11-25 16:56:03 -0500

    Fixed a problem with pulling from the nonexistent columns in the lsf table.

--- app/controllers/main_routes/studentOverloadApp.py
+++ app/controllers/main_routes/studentOverloadApp.py
@@ -11,7 +11,7 @@ def studentOverloadApp(formId):
     current_user = require_login()
     if not current_user:        # Not logged in
         return render_template('errors/403.html')
-
+    print(1)
     overloadForm = LaborStatusForm.get(LaborStatusForm.laborStatusFormID == formId)
     prefillStudentName = overloadForm.studentSupervisee.FIRST_NAME + " "+ overloadForm.studentSupervisee.LAST_NAME
     prefillStudentBnum = overloadForm.studentSupervisee.ID
@@ -26,15 +26,19 @@ def studentOverloadApp(formId):
     todayYear = today.year
     termYear = todayYear * 100
     currentAcademicTerm = Term.select().where(Term.termCode.between(termYear-1, termYear + 15))
+    print(2)
     for term in currentAcademicTerm:
         listOfTerms.append(term)
+    print(3)
     print(listOfTerms)
-    studentSpecificLabor = LaborStatusForm.select(LaborStatusForm.laborStatusFormID).where(LaborStatusForm.studentSupervisee.ID == prefillStudentBnum, LaborStatusForm.jobType == "Primary")
+    studentSpecificLabor = LaborStatusForm.select(LaborStatusForm.laborStatusFormID).where(LaborStatusForm.studentSupervisee_id == prefillStudentBnum, LaborStatusForm.jobType == "Primary")
+    print(4)
     for i in studentSpecificLabor:
         print(i, "Im here")
+    print(5)
     studentSpecificHistory = FormHistory.select().where((FormHistory.formID == '%s' %i) for i in studentSpecificLabor)
     studentHistory = studentSpecificHistory.where(
-                            ((studentSpecificHistory.status == "Approved") | (studentSpecificHistory.status == "Approved Reluctantly"))
+                            ((studentSpecificHistory.status_id == "Approved") | (studentSpecificHistory.status == "Approved Reluctantly"))
     ).get()
     print(studentHistory)
 
