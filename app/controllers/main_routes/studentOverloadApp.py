2c09f4bbb881b20b66f28932ed791200a2d7217a
Ela <ela.jamali09@gmail.com>
2019-12-09 17:10:14 -0500

    working on the post method

--- app/controllers/main_routes/studentOverloadApp.py
+++ app/controllers/main_routes/studentOverloadApp.py
@@ -95,9 +95,14 @@ def updateDatabase():
         rsp = eval(request.data.decode("utf-8"))
         if rsp:
             formId = rsp.keys()
+            print(type(formId))
             for data in rsp.values():
                 print(data)
-                overloadform = OverloadForm.create(overloadReason = data["Notes"])
+                overloadForm = OverloadForm.create(overloadReason = data["Notes"])
+                formHistoryForm = FormHistory.get(FormHistory.formHistoryID == formId[0])
+                print(formHistoryForm)
+                formHistoryForm.overloadForm = str(overloadForm.overloadFormID)
+                formHistoryForm.save()
         return jsonify({"Success": True})
     except Exception as e:
         print("ERROR: " + str(e))
