170ee15bdcbbc83dcd79c2f7d6fe697dea4a7060
Hila Manalai <manalaih@berea.edu>
2019-12-16 17:13:04 -0500

    worked on getting student information and styling the pdf

--- app/controllers/main_routes/laborHistory.py
+++ app/controllers/main_routes/laborHistory.py
@@ -136,13 +136,23 @@ def populateModal(statusKey):
         return render_template('errors/500.html')
         return (jsonify({"Success": False}))
 
-@main_bp.route('/laborHistory/modal/printPdf/<statusKey>', methods=['POST'])
-def ConvertToPDF():
-    forms = FormHistory.select().where(FormHistory.formID == statusKey)
-    pdf = render_template('snips/pdfTemplate.html',
-                    forms = forms,
-                    )
-    return pdf
+@main_bp.route('/laborHistory/modal/printPdf/<statusKey>', methods=['GET'])
+def ConvertToPDF(statusKey):
+    """
+    This function returns 
+    """
+    try:
+        forms = FormHistory.select().where(FormHistory.formID == statusKey).order_by(FormHistory.createdDate.desc())
+        statusForm = LaborStatusForm.select().where(LaborStatusForm.laborStatusFormID == statusKey)
+        pdf = make_response(render_template('snips/pdfTemplate.html',
+                        forms = forms,
+                        statusForm = statusForm
+                        ))
+        return (pdf)
+    except Exception as e:
+        return render_template('errors/500.html')
+        return(jsonify({"Success": False}))
+
 
 @main_bp.route('/laborHistory/modal/updatestatus', methods=['POST'])
 def updatestatus_post():
--- app/static/js/laborhistory.js
+++ app/static/js/laborhistory.js
@@ -1,69 +1,23 @@
 function fillPDF(laborStatusKey){
+  /* This function gets a response from controller function: ConvertToPDF() in laborHistory.py. The response is an HTML Template
+     that is converted to pdf using jsPDF*/
   $.ajax({
     type: "GET",
-    url: '/laborHistory/modal/printPdf' + laborStatusKey,
+    url: '/laborHistory/modal/printPdf/' + laborStatusKey,
     success: function(response) {
-
-      //alert(response);
-      var doc = new jsPDF();
-      doc.fromHTML(response, 20, 170, {'width':155});
+      var doc = new jsPDF('portrait', 'pt', 'letter') // Create a new jsPDF document
+      margins = {
+       top: 40,
+       bottom: 60,
+       left: 40,
+       width: 522
+       };
+      doc.setTextColor(0,90,133);
+      doc.fromHTML(response, margins.left,margins.top, {width: margins.width}); // Change the HTML template to PDF
       doc.save();
 
     }
   });
-
-
-
-  //alert("Hi")
-  // var doc = new jsPDF();
-  // doc.setTextColor(43, 66, 99);
-  // doc.setFontType("bold");
-  // doc.setFontSize(15);
-  // doc.text(20, 70, 'STATUS FORM')
-  // doc.text(20, 80, "Labor Position Participation Agreement");
-  // doc.setFontSize(9);
-  // doc.setFontType("normal");
-  // var loremipsum =  "The purpose of this form is to establish student work status during an academic term (Fall, Spring, or Summer), during a break period (Thanksgiving, Christmas, Spring Break), or for a summer practicume assginment."
-  // lines = doc.splitTextToSize(loremipsum, 170);
-  // doc.text(20, 90, lines);
-  // doc.setFontSize(15);
-  // doc.setFontType("bold");
-  // doc.text(20, 120, "Primary Contract");
-  // doc.setFontSize(15);
-  // doc.text(20, 170, "Guildlines");
-  // doc.setFontType("normal");
-  // doc.setFontSize(10);
-  // doc.text(20, 180, "Primary: ");
-  // var primText = " Positions through which a student meets the primary labor requirement during an academic term or working a summer practicum assignment. All students sign a Labor Enrollment Agreement upon admission to Berea College. In signing this form, each student agrees to"
-  // lines_2 = doc.splitTextToSize(primText, 170);
-  // doc.text(20, 185, lines_2);
-  //
-  // var primList_1 = "work no less than 10 hours a week and adhere to the work schedule as required by this position and arranged with the supervisor"
-  // var primList_2 = "work any additional hours as defined in this status form including adhering to the work schedule as required by the position"
-  // var primList_3= "secure approval for working continuously more than 15 hours per week as specified in the Labor overload approval process"
-  //
-  //
-  // doc.fromHTML("<ul><li>"+ primList_1+ "</li></ul>", 20, 200, {'width':155});
-  // doc.fromHTML("<ul><li>"+ primList_2+"</li></ul>", 20, 210, {'width':155});
-  // doc.fromHTML("<ul><li>"+ primList_3+"</li></ul>", 20,220, {'width':155});
-  //
-  // doc.setFontSize(10);
-  // doc.text(20, 230, "Secondary: ");
-  // var seconText = "Positon taken by a student in addition to his/her primary position. Students may take secondary jobs as a means of earning extra income, learning a new skill, participating in a desired program, or providing an important service. Primary supervisors must approve secondary positions and they may not interfere with the student's primary performance."
-  // lines_6 = doc.splitTextToSize(seconText, 170);
-  // doc.text(20, 235, lines_6);
-  //
-  // doc.setFontSize(10);
-  // doc.setFontType("bold");
-  // doc.text(20, 255, "SUPERVISOR CHECKLIST:");
-  //
-  // doc.fromHTML("<ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul>",  20, 260);
-  // //  doc.output("dataurlnewwindow");
-  //
-  //
-  // doc.save();
-  // //doc.autoPrint()
-
 }
 
 function openModal(laborStatusKey) {
@@ -75,7 +29,6 @@ function openModal(laborStatusKey) {
     type: "GET",
     url: '/laborHistory/modal/' + laborStatusKey,
     success: function(response) {
-      alert(response);
       $("#holdModal").empty().append(response);
       $("#modal").modal("show");
       $("#modify").attr("href", "/modifyLSF/" + laborStatusKey); // will go to the modifyLSF controller
