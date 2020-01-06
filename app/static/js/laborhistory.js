37d4fafca1494f184dae0743e96e01ef652ad0c5
Ela <ela.jamali09@gmail.com>
2020-01-06 16:33:09 -0500

    I tried to fix the modal issue but this is borken now.

--- app/static/js/laborhistory.js
+++ app/static/js/laborhistory.js
@@ -1,16 +1,28 @@
+// $(document).on('click','.accept', function(e){
+//      $(".modal-fade").modal("hide");
+//      $(".modal-backdrop").remove();
+//  });
 function openModal(laborStatusKey) {
   /*
     This function gets a response from the controller function: populateModal() in laborHistory.py.  The response is the data for the modal that pops up
     when the position is clicked.
   */
-
+  // $(".modal-fade").modal("hide");
+  // $(".modal-backdrop").remove();
   $.ajax({
     type: "GET",
     url: '/laborHistory/modal/' + laborStatusKey,
     success: function(response) {
       //console.log(response);
       $("#holdModal").empty().append(response);
-      $("#modal").modal("show");
+      // $("#modal").modal({backdrop: "false"});
+    //   angular.run(function($rootScope) {
+    //   $rootScope.$on('$routeChangeStart', destroyTheBackdrop);
+    //
+    //   function destroyTheBackdrop() {
+    //     $('.modal-backdrop').hide();
+    //   }
+    // });
       $("#modify").attr("href", "/modifyLSF/" + laborStatusKey); // will go to the modifyLSF controller
       $("#rehire").attr("href", "/laborstatusform/" + laborStatusKey); // will go to the lsf controller
       $("#release").attr("href", "/laborReleaseForm/" + laborStatusKey); // will go to labor release form controller
@@ -19,13 +31,36 @@ function openModal(laborStatusKey) {
 }
 // prevent a second click for 2 second.
 function preventDoubleClick(id){
-  onclick_attr = $("#" + id).attr("onclick");
-  $("#" + id).attr("onclick","");
+  console.log("I am clicked")
+  $("#positionTable").off('click'); //disables click event
+  $("#modal").modal("show");
   setTimeout(function(){
-    $("#" + id).attr("onclick",onclick_attr);
-  }, 2000);
-  return false;
+    //To re-enable:
+    $("#positionTable").bind("click",eventhandler);}, 1000);
 }
+
+
+  // var clickid = document.getElementById("positionTable")
+  // console.log(clickid)
+  // $(".modalLink").attr("onclick","");
+  // // clickid.onclick = null
+  // console.log(clickid)
+  // setTimeout(fucntion(){
+  //   clickid.onlclick = preventDoubleClick;
+  //   $("#modal").modal("show");
+  // }, 1000);
+
+  // onclick_attr = $("#" + id).attr("onclick");
+  // // onclicks = $(".modalLink").map(function(el) {
+  // //   return el.attr("onclick")
+  // // });
+  //
+  // $("#" + id).attr("onclick","");
+  // setTimeout(function(){
+  //   $("#" + id).attr("onclick",onclick_attr);
+  // }, 2000);
+  // return false;
+
 
 function withdrawform(formID){
   /*
