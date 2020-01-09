3cade3f1eb20e67342995456ebeb8068cdbeb6fb
Guillermo Cruz <guillermo_cruz@berea.edu>
2020-01-09 14:41:39 -0500

    Removed search box from select picker, made the Purpose field required, fixed the wording on modal, and fixed the title on select pickers.

--- app/static/js/emailTemplates.js
+++ app/static/js/emailTemplates.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
-  // CKEDITOR.replace( 'editor1' );
 });
 
 function populatePurpose(){
-  // alert("Hello world")
   $("#purpose").val('default').selectpicker("refresh");
   $("#purpose").empty();
   $("#subject").val("Subject")
   CKEDITOR.instances["editor1"].setData('');
   var recipient = $("#recipient").val();
-  console.log(recipient);
   $.ajax({
     url: "/admin/emailTemplates/getPurpose/" + recipient,
     dataType: "json",
     success: function(response){
-      console.log(response)
       for (var key in response){
         var value = response[key]["Purpose"]
         $("#purpose").append('<option value="' + value + '">' + value + '</option>');
@@ -28,14 +24,12 @@ function getEmailTemplate(){
   $("#subject").val("Subject")
   CKEDITOR.instances["editor1"].setData('');
   var purpose = $("#purpose").val();
-  console.log(purpose);
   $.ajax({
     url: "/admin/emailTemplates/getEmail/" + purpose,
     dataType: "json",
     success: function(response){
       var body = response["emailBody"]
       var subject = response["emailSubject"]
-      console.log(subject);
       CKEDITOR.instances["editor1"].insertHtml(body);
       $("#subject").val(subject)
     }
@@ -45,8 +39,6 @@ function getEmailTemplate(){
 function postEmailTemplate(){
   var body = CKEDITOR.instances.editor1.getData();
   var purpose = $("#purpose").val();
-  console.log(body);
-  console.log(purpose);
   $.ajax({
     url: "/admin/emailTemplates/postEmail/",
     data: { 'body': body, 'purpose': purpose },
@@ -55,28 +47,35 @@ function postEmailTemplate(){
     success: function(response){
       location.reload()
     }
-
   })
-  // msg = "The email template has been successfully updated.";
-  // category = "success";
-  // $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>');
-  // $("#flasher").delay(3000).fadeOut();
 }
 
 function discard(){
-  $("#recipient").val('default').selectpicker("refresh");
-  $("#purpose").val('default').selectpicker("refresh");
-  $("#purpose").empty();
-  $("#subject").val("Subject")
-  CKEDITOR.instances["editor1"].setData('');
-  msg = "The email template changes have been discarded.";
-  category = "info";
+  if ( !$("#purpose").val() ) {
+    msg = "There are no changes to be discarded.";
+    category = "danger";
+  }
+  else {
+    $("#recipient").val('default').selectpicker("refresh");
+    $("#purpose").val('default').selectpicker("refresh");
+    $("#purpose").empty();
+    $("#subject").val("Subject")
+    CKEDITOR.instances["editor1"].setData('');
+    msg = "The email template changes have been discarded.";
+    category = "info";
+  }
   $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>');
   $("#flasher").delay(3000).fadeOut();
 }
 
-$(function() {
-   $("#submit").click(function() {
-      $("#modal").modal("show");
-   });
-});
+function saveChanges() {
+  if ( !$("#purpose").val() ) {
+    msg = "There are no changes to be saved.";
+    category = "danger";
+    $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>');
+    $("#flasher").delay(3000).fadeOut();
+  }
+  else{
+    $("#modal").modal("show");
+  }
+}
