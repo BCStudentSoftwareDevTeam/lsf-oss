12e2ff6187391bde03cead7658043cd48df22492
Otienoa <otienoa@berea.edu>
2020-01-30 12:03:41 -0500

    Started working on the controller to get data from overload form.

--- app/controllers/admin_routes/financialAidOverload.py
+++ app/controllers/admin_routes/financialAidOverload.py
@@ -8,63 +8,64 @@ from flask import json, jsonify
 from flask import request, render_template
 from app.models.overloadForm import *
 
-@admin.route('/admin/financialAidOverloadApproval/<overloadFormID>')
+@admin.route('/admin/financialAidOverloadApproval/<overloadFormID>', methods=['GET']) # the form ID here is the ID from overloadForm table
 def financialAidOverload(overloadFormID):
-    return render_template('admin/financialAidOverload.html')
+
+    overloadForm = FormHistory.get(FormHistory.formHistoryID == overloadFormID)
+    lsfForm = LaborStatusForm.get(LaborStatusForm.laborStatusFormID == overloadForm.formID)
+    # the following lines prefills the information for student
+    studentName = lsfForm.studentSupervisee.FIRST_NAME + " "+ lsfForm.studentSupervisee.LAST_NAME
+    studentBnum = lsfForm.studentSupervisee.ID
+    department = lsfForm.department.DEPT_NAME
+    position = lsfForm.POSN_TITLE
+    overloadHours= lsfForm.weeklyHours
+    termCodeYear = Term.select(Term.termCode).where(Term.termCode.between(termYear-1, termYear + 15))
+    currentTerm = str(lsfForm.termCode.termCode)[-2:]
+    TermsNeeded=[]
+
+    for term in termCodeYear:
+        if str(term)[-2:] == "11" or str(term)[-2:] == "12" or str(term)[-2:]== "00":
+            TermsNeeded.append(term)
+
+    studentPrimaryLabor = LaborStatusForm.select(LaborStatusForm.laborStatusFormID).where(LaborStatusForm.studentSupervisee_id == studentBnum,
+                                                                                           LaborStatusForm.jobType == "Primary",
+                                                                                           LaborStatusForm.termCode.in_(TermsNeeded))
+
+    studentSecondaryLabor = LaborStatusForm.select(LaborStatusForm.laborStatusFormID).where(LaborStatusForm.studentSupervisee_id == studentBnum,
+                                                                                               LaborStatusForm.jobType == "Secondary",
+                                                                                               LaborStatusForm.termCode.in_(TermsNeeded))
+    formIDPrimary = []
+    for i in studentPrimaryLabor:
+        studentPrimaryHistory = FormHistory.select().where((FormHistory.formID == i) & (FormHistory.historyType == "Labor Status Form") & ((FormHistory.status == "Approved") | (FormHistory.status == "Approved Reluctantly") | (FormHistory.status == "Pending")))
+        formIDPrimary.append(studentPrimaryHistory)
+    formIDSecondary = []
+    for i in studentSecondaryLabor:
+        studentSecondaryHistory = FormHistory.select().where((FormHistory.formID == i) & (FormHistory.historyType == "Labor Status Form") & ((FormHistory.status == "Approved") | (FormHistory.status == "Approved Reluctantly") | (FormHistory.status == "Pending")))
+        formIDSecondary.append(studentSecondaryHistory)
+    totalCurrentHours = 0
+    for i in formIDPrimary:
+        for j in i:
+            if str(j.status) != "Pending":
+                totalCurrentHours += j.formID.weeklyHours
+    for i in formIDSecondary:
+        for j in i:
+            if str(j.status) != "Pending":
+                totalCurrentHours += j.formID.weeklyHours
+    totalFormHours = totalCurrentHours + overloadHours
 
 
-    # studentPrimaryLabor = LaborStatusForm.select(LaborStatusForm.laborStatusFormID).where(LaborStatusForm.studentSupervisee_id == prefillStudentBnum,
-    #                                                                                        LaborStatusForm.jobType == "Primary",
-    #                                                                                        LaborStatusForm.termCode.in_(TermsNeeded))
-    # formIDPrimary = []
-    # for i in studentPrimaryLabor:
-    #     studentPrimaryHistory = FormHistory.select().where((FormHistory.formID == i) & (FormHistory.historyType == "Labor Status Form") & ((FormHistory.status == "Approved") | (FormHistory.status == "Approved Reluctantly") | (FormHistory.status == "Pending")))
-    #     formIDPrimary.append(studentPrimaryHistory)
-    # formIDSecondary = []
-    # for i in studentSecondaryLabor:
-    #     studentSecondaryHistory = FormHistory.select().where((FormHistory.formID == i) & (FormHistory.historyType == "Labor Status Form") & ((FormHistory.status == "Approved") | (FormHistory.status == "Approved Reluctantly") | (FormHistory.status == "Pending")))
-    #     formIDSecondary.append(studentSecondaryHistory)
-    # totalCurrentHours = 0
-    # for i in formIDPrimary:
-    #     for j in i:
-    #         if str(j.status) != "Pending":
-    #             totalCurrentHours += j.formID.weeklyHours
-    # for i in formIDSecondary:
-    #     for j in i:
-    #         if str(j.status) != "Pending":
-    #             totalCurrentHours += j.formID.weeklyHours
-    # totalFormHours = totalCurrentHours + prefillHoursOverload
-    # return render_template( 'main/studentOverloadApp.html',
-	# 			            title=('student Overload Application'),
-    #                         username = current_user,
-    #                         overloadForm = overloadForm,
-    #                         prefillStudentName = prefillStudentName,
-    #                         prefillStudentBnum = prefillStudentBnum,
-    #                         prefillStudentCPO = prefillStudentCPO,
-    # #                         prefillStudentClass = prefillStudentClass,
-    #                         prefillTerm = prefillTerm,
-    #                         prefillDepartment = prefillDepartment,
-    #                         prefillPosition = prefillPosition,
-    #                         prefillHoursOverload = prefillHoursOverload,
-    #                         currentPrimary = formIDPrimary,
-    #                         currentSecondary = formIDSecondary,
-    #                         totalCurrentHours = totalCurrentHours,
-    #                         totalFormHours = totalFormHours
-    #                       )
-#
-# @main_bp.route('/studentOverloadApp/update', methods=['POST'])
-# def updateDatabase():
-#     try:
-#         # NEED TO ADD CURRENT PRIMARY AND CURRENT SECONDARY AFTER THE MIGRATION
-#         rsp = eval(request.data.decode("utf-8"))
-#         if rsp:
-#             formId = rsp.keys()
-#             for data in rsp.values():
-#                 overloadForm = OverloadForm.create(overloadReason = data["Notes"])
-#                 formHistoryForm = FormHistory.get(FormHistory.formHistoryID == data["formID"])
-#                 formHistoryForm.overloadForm = overloadForm.overloadFormID
-#                 formHistoryForm.save()
-#         return jsonify({"Success": True})
-#     except Exception as e:
-#         print("ERROR: " + str(e))
-#         return jsonify({"Success": False})
+
+# will need to add term to the interface and then have a prefill variable
+    return render_template( 'admin/financialAidOverload.html',
+                        username = current_user,
+                        overloadForm = overloadForm,
+                        studentName = studentName,
+                        studentBnum = studentBnum,
+                        department = department,
+                        position = position,
+                        overloadHours = overloadHours,
+                        currentPrimary = formIDPrimary,
+                        currentSecondary = formIDSecondary,
+                        totalCurrentHours = totalCurrentHours,
+                        totalFormHours = totalFormHours
+                      )
