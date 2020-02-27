ee626d8f50a7ebff6607d0e73cf8d7a57ee3f9e7
Brian Ramsay <ramsayb2@berea.edu>
2020-02-27 13:36:44 -0500

    Add object to handle banner db access (it might not work yet)

--- app/logic/banner.py
+++ app/logic/banner.py
@@ -8,20 +8,21 @@ class banner():
     def __init__(self):
         secret_conf = get_secret_cfg()
 
-        app.config["BANNER_PASSWORD"] = secret_conf['BANNER_PASSWORD']
+        app.config["banner"]["password"] = secret_conf['BANNER_PASSWORD']
+        banner_cfg = app.config["banner"]
 
         self.database_exists = False
         if env == 'production':
             self.database_exists = True
             try:
-                self.conn = cx_Oracle.connect(
-                        app.config["BANNER_USER"],
-                        app.config["BANNER_PASSWORD"], 
-                        app.config["BANNER_SERVER"])
-                print(f"BANNER connection initialized. Oracle version {self.conn.version}")
+                self.conn = cx_Oracle.connect( 
+                        banner_cfg["user"], 
+                        banner_cfg["password"], 
+                        "{url}:{port}/{sid}".format(**banner_cfg))
+                print("BANNER connection initialized. Oracle version {}".format(self.conn.version))
 
             except Exception as err:
-                print(f"BANNER connection failed: {err}")
+                print("BANNER connection failed: {}".format(err))
                 self.database_exists = False
 
     def canConnect:
@@ -29,15 +30,22 @@ class banner():
 
     def query(sql):
         if self.conn:
-            cursor = self.conn.cursor()
-            cursor.execute(sql)
-            return cursor.fetchall()
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(sql)
+                return cursor.fetchall()
+            except Exception as err:
+                print("Error querying BANNER db:", err)
+                return []
         return []
 
-    def insert(data):
+    def insert(sql):
         if self.conn:
-            cursor = self.conn.cursor()
-            cursor.execute(sql)
+            try:
+                cursor = self.conn.cursor()
+                return cursor.execute(sql)
+            except Exception as err:
+                print("Error inserting into BANNER db:", err)
+                return False
         else:
-            return True
-
+            return None # The same as a succesful execution
