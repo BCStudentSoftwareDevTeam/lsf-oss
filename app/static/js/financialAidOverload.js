a0dc78689a7df1e3b87268bb2662510c00466137
Hila Manalai <manalaih@berea.edu>
2020-05-14 12:18:20 -0400

    adding a function to check required fields

--- app/static/js/financialAidOverload.js
+++ app/static/js/financialAidOverload.js
@@ -1,31 +1,37 @@
-function denialSubmission(formHistoryKey){
+function overloadSubmission(formHistoryKey){
   // this function will update the notes and status in the db
-  console.log("in denial submission");
-  console.log(formHistoryKey, "formHistoryKey");
   var notesContent = $("#denyReason_"+formHistoryKey).val()
-  console.log(notesContent, "deny note content");
   denialInfoDict = {}
   denialInfoDict["formHistoryID"] = formHistoryKey
   denialInfoDict["denialNote"] = notesContent
-  console.log(denialInfoDict, "DenialDict");
   data = JSON.stringify(denialInfoDict)
-  $.ajax({
-    method: "POST",
-    url:"/admin/financialAidOverloadApproval/denial",
-    datatype: "json",
-    data: data,
-    contentType: "application/json",
-    success:function(){
-      console.log("success");
-      msgFlash("Your changes have been saved successfuly.(You will be redirected shortly.)", "success")
-      setTimeout(function() { // executed after 1 second
-         window.location.replace('http://berea.edu'); // redirects to a new website
-       }, 5000);
-    },
-    error:function(response){
-      console.log("error", response);
-    }
-  })
+
+  if($(".textarea-required").prop("required") == true && notesContent == " "){
+    $("#finOverloadSubmit").removeAttr("data-dismiss")
+    $(".textarea-required").focus();
+    $("#required-error").show();
+  }
+  else{
+    $("#required-error").hide();
+    $("#finOverloadSubmit").attr("data-dismiss", "modal")
+    $.ajax({
+      method: "POST",
+      url:"/admin/financialAidOverloadApproval/denial",
+      datatype: "json",
+      data: data,
+      contentType: "application/json",
+      success:function(){
+        console.log("success");
+        msgFlash("Your changes have been saved successfuly.(You will be redirected shortly.)", "success")
+        setTimeout(function() { // executed after 1 second
+           window.location.replace('http://berea.edu'); // redirects to a new website
+         }, 5000);
+      },
+      error:function(response){
+        console.log("error", response);
+      }
+    });
+  }
 }
 
 // for showing different messages with flash
@@ -41,4 +47,21 @@ function msgFlash(flash_message, status){
         $("#flasher").delay(5000).fadeOut();
     }
 
+}
+
+function openApproveDenyModal(status){
+  if (status == "approved"){
+    $("#required-error").hide();
+    $("#finOverloadSubmit").attr("data-dismiss", "modal")
+    $("#finOverloadModal .modal-title").text("Reason for Approval")
+    $("#modal-body-content").text("You have selected 'Approve' for this student's Overload Request. Please indicate a reasoning for this decision (Optional).");
+    $(".textarea-required").prop('required', false);
+    $("#finOverloadModal").modal("show");
+  }
+  else{
+    $("#finOverloadModal .modal-title").text("Reason for Denial");
+    $("#modal-body-content").text("You have selected 'Deny' for this student's Overload Request. Please indicate a reasoning for this decision");
+    $(".textarea-required").prop('required', true);
+    $("#finOverloadModal").modal("show");
+  }
 }
