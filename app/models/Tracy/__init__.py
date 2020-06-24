df3e9bea67832600e6b58fe1ee5038087bd8bfcc
Brian Ramsay <ramsayb2@berea.edu>
2020-06-24 19:03:27 -0400

    SQL Server connection works for production

--- app/models/Tracy/__init__.py
+++ app/models/Tracy/__init__.py
@@ -1,9 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import load_config, app
+import os
 
 cfg = load_config('app/config/secret_config.yaml')
-# production mssql+pyodbc_mssql
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{}:{}@{}/{}".format(cfg['tracy']['username'], cfg['tracy']['password'], 'localhost', cfg['tracy']['db_name'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+host = cfg['tracy']['host']
+if not os.environ.get("USING_CONTAINER", False):
+    host = "localhost"
+
+# MySQL database connection
+uri = "mysql+pymysql://{}:{}@{}/{}".format(cfg['tracy']['username'], cfg['tracy']['password'], host, cfg['tracy']['db_name'])
+
+# MSSQL database connection
+if app.config['ENV'] == 'production':
+    uri = "mssql+pyodbc:///?odbc_connect=" + quote('DRIVER=FreeTDS;SERVER={};PORT=1433;DATABASE={};UID={};PWD={};TDS_Version=8.0;'.format(cfg['tracy']['mssql_host'],  cfg['tracy']['db_name'], cfg['tracy']['mssql_user'], cfg['tracy']['mssql_password']))
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
+
