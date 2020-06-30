53130dc4ac0ccd4e6fb2c8be81c1a5724e386f32
Brian Ramsay <ramsayb2@berea.edu>
2020-06-30 10:05:43 -0400

    Fix spelling error in comment

--- app/logic/absolute_path.py
+++ app/logic/absolute_path.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 '''Creates the AbsolutePath based off of the relative path.
 Also creates the directories in path if they are not found.
-@param {string} relaitivePath - a string of directories found in config.yaml
+@param {string} relativePath - a string of directories found in config.yaml
 @param {string} filename - the name of the file that should be in that directory
-@return {string} filepath -returns the absolute path of the directory'''
+@return {string} filepath - returns the absolute path of the directory'''
 '''TODO: ADD @PARAm for make dirs'''
-def getAbsolutePath(relativePath,filename=None,makeDirs=False):
+def getAbsolutePath(relativePath, filename=None, makeDirs=False):
     filepath = os.path.join(Path(__file__).parent.parent.parent,relativePath)
     if makeDirs == True:
         try:
