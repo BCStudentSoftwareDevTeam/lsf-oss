12c6ad831fa678d9cff1eb18a7b2b040ce2ac572
Hila Manalai <manalaih@berea.edu>
2020-07-13 15:05:31 -0400

    changed a wrong conditional

--- app/controllers/main_routes/laborHistory.py
+++ app/controllers/main_routes/laborHistory.py
@@ -122,11 +122,8 @@ def populateModal(statusKey):
             if currentUser.Student and currentUser.username == student.username:
                 buttonState = ButtonStatus.show_student_labor_eval_button
                 break
-            # elif currentUser.Supervisor.ID != (form.createdBy.Supervisor.ID or form.formID.supervisor.ID):
-            #     buttonState = ButtonStatus.no_buttons # otherwise, show the notification
-            #     break
-            elif currentUser.Supervisor.ID != form.formID.supervisor.ID:
-                buttonState = ButtonStatus.no_buttons
+            elif (currentUser.Supervisor.ID != form.createdBy.Supervisor.ID) and (currentUser.Supervisor.ID != form.formID.supervisor.ID):
+                buttonState = ButtonStatus.no_buttons # otherwise, show the notification
                 break
             else:
                 if form.releaseForm != None:
