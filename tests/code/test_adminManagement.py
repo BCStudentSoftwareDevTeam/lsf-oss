84a2dae96d3e7391b1021430a7b52e7c6796d1b3
Sreynit Khatt <sreynitkhatt@macbook-pro.home>
2020-07-20 17:48:25 -0400

    created negative test suites

--- tests/code/test_adminManagement.py
+++ tests/code/test_adminManagement.py
@@ -5,38 +5,38 @@ from peewee import DoesNotExist
 
 @pytest.mark.integration
 def test_addAdmin():
-    isAdmin = 1
+    isAdmin = 0
 
     newAdmin = "pearcej"
     user = User.get(User.username == newAdmin)
 
     # Test adding labor admin
     addAdmin(user, labor='labor')
-    assert user.isLaborAdmin == isAdmin
+    assert user.isLaborAdmin != isAdmin
 
     # Test adding financial aid admin
     addAdmin(user, finAid='Financial Aid')
-    assert user.isFinancialAidAdmin == isAdmin
+    assert user.isFinancialAidAdmin != isAdmin
 
     # Test adding saas admin
     addAdmin(user, saas='SAAS')
-    assert user.isSaasAdmin == isAdmin
+    assert user.isSaasAdmin != isAdmin
 
 @pytest.mark.integration
 def test_removeAdmin():
-    isAdmin = 0
+    isAdmin = 1
 
     oldAdmin = "pearcej"
     user = User.get(User.username == oldAdmin)
 
     # Test removing labor admin
     removeAdmin(user, labor= 'labor')
-    assert user.isLaborAdmin == isAdmin
+    assert user.isLaborAdmin != isAdmin
 
     # Test removing financial aid admin
     removeAdmin(user, finAid= 'Financial Aid')
-    assert user.isFinancialAidAdmin == isAdmin
+    assert user.isFinancialAidAdmin != isAdmin
 
     # Test removing saas admin
     removeAdmin(user, saas= 'SAAS')
-    assert user.isSaasAdmin == isAdmin
+    assert user.isSaasAdmin != isAdmin
