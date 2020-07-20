8c302cdc5900eefc6582c64ed06bf1eba5233348
Sreynit Khatt <sreynitkhatt@macbook-pro.home>
2020-07-20 16:34:49 -0400

    created more test suites

---
+++ tests/code/test_adminManagement.py
@@ -0,0 +1,42 @@
+import pytest
+from app.controllers.admin_routes.adminManagement import addAdmin, removeAdmin
+from app.models.user import User
+from peewee import DoesNotExist
+
+@pytest.mark.integration
+def test_addAdmin():
+    isAdmin = 1
+
+    newAdmin = "pearcej"
+    user = User.get(User.username == newAdmin)
+
+    # Test adding labor admin
+    addAdmin(user, labor='labor')
+    assert user.isLaborAdmin == isAdmin
+
+    # Test adding financial aid admin
+    addAdmin(user, finAid='Financial Aid')
+    assert user.isFinancialAidAdmin == isAdmin
+
+    # Test adding saas admin
+    addAdmin(user, saas='SAAS')
+    assert user.isSaasAdmin == isAdmin
+
+@pytest.mark.integration
+def test_removeAdmin():
+    isAdmin = 0
+
+    oldAdmin = "pearcej"
+    user = User.get(User.username == oldAdmin)
+
+    # Test removing labor admin
+    removeAdmin(user, labor= 'labor')
+    assert user.isLaborAdmin == isAdmin
+
+    # Test removing financial aid admin
+    removeAdmin(user, finAid= 'Financial Aid')
+    assert user.isFinancialAidAdmin == isAdmin
+
+    # Test removing saas admin
+    removeAdmin(user, saas= 'SAAS')
+    assert user.isSaasAdmin == isAdmin
