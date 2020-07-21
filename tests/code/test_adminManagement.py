1f865656e9b11b2b47b024e0d83f2a3876ed4645
Hila Manalai <manalaih@berea.edu>
2020-07-21 09:01:52 -0400

    remove variable

--- tests/code/test_adminManagement.py
+++ tests/code/test_adminManagement.py
@@ -5,38 +5,34 @@ from peewee import DoesNotExist
 
 @pytest.mark.integration
 def test_addAdmin():
-    isAdmin = 0
-
     newAdmin = "pearcej"
     user = User.get(User.username == newAdmin)
 
     # Test adding labor admin
     addAdmin(user, 'labor')
-    assert user.isLaborAdmin != isAdmin
+    assert user.isLaborAdmin != 0
 
     # Test adding financial aid admin
     addAdmin(user, 'finAid')
-    assert user.isFinancialAidAdmin != isAdmin
+    assert user.isFinancialAidAdmin != 0
 
     # Test adding saas admin
     addAdmin(user, 'saas')
-    assert user.isSaasAdmin != isAdmin
+    assert user.isSaasAdmin != 0
 
 @pytest.mark.integration
 def test_removeAdmin():
-    isAdmin = 1
-
     oldAdmin = "pearcej"
     user = User.get(User.username == oldAdmin)
 
     # Test removing labor admin
     removeAdmin(user, 'labor')
-    assert user.isLaborAdmin != isAdmin
+    assert user.isLaborAdmin != 1
 
     # Test removing financial aid admin
     removeAdmin(user, 'finAid')
-    assert user.isFinancialAidAdmin != isAdmin
+    assert user.isFinancialAidAdmin != 1
 
     # Test removing saas admin
     removeAdmin(user, 'saas')
-    assert user.isSaasAdmin != isAdmin
+    assert user.isSaasAdmin != 1
