27372f7231d7746f55cc819e5795ea7fe038aa63
Hila Manalai <manalaih@berea.edu>
2020-07-22 10:12:55 -0400

    adding tests

--- tests/code/test_alterLSF.py
+++ tests/code/test_alterLSF.py
@@ -6,16 +6,36 @@ from app.models.adminNotes import AdminNotes
 from app.models.adjustedForm import AdjustedForm
 from app.models.formHistory import FormHistory
 
+#  TODO : At the beginning of the file clean up the records we are creating.
+@pytest.fixture
+def setup():
+    db_cleanup()
+    yield
+
+@pytest.fixture
+def cleanup():
+    yield
+    db_cleanup()
+
+def db_cleanup():
+    AdminNotes.delete().where(AdminNotes.formID.cast('char').contains("9")).execute()
+    FormHistory.delete().where((FormHistory.formID == 9) & (FormHistory.historyType != 'Labor Status Form')).execute()
+    # AdjustedForm.delete().where(AdjustedForm.formID == 8).execute()
+
+
+
 currentUser = User.get(User.userID == 1) # Scott Heggen's entry in User table
-LSF = LaborStatusForm.get(LaborStatusForm.laborStatusFormID == 2)
+LSF = LaborStatusForm.get(LaborStatusForm.laborStatusFormID == 9)
 rsp = {'supervisor':{'oldValue':'B12361006', 'newValue':'B12365892','date':'07/21/2020'},
-       'weeklyHours':{'oldValue': '20', 'newValue': '10', 'date': '07/21/2020'},
+       'weeklyHours':{'oldValue': '10', 'newValue': '12', 'date': '07/21/2020'},
        'position':{'oldValue': 'S61419', 'newValue': 'S61407', 'date': '07/21/2020'},
        'supervisorNotes':{'oldValue':'old notes.', 'newValue':'new notes.'}
        }
 
+rsp_overload = {'weeklyHours': {'oldValue':'10', 'newValue':'20'}}
+
 @pytest.mark.integration
-def test_modifyLSF():
+def test_modifyLSF(setup, cleanup):
 
     k = 'supervisorNotes'
     modifyLSF(rsp, k, LSF, currentUser)
@@ -33,8 +53,13 @@ def test_modifyLSF():
     modifyLSF(rsp, k, LSF, currentUser)
     assert LSF.weeklyHours == 10
 
+    # # Overload hours created
+    # k = 'weeklyHours'
+    # modifyLSF(rsp_overload, k, LSF, currentUser)
+    # assert LSF.weeklyHours == 20
+
 @pytest.mark.integration
-def test_adjustLSF():
+def test_adjustLSF(setup, cleanup):
 
     k = 'supervisorNotes'
     adjustLSF(rsp, k, LSF, currentUser)
@@ -46,14 +71,25 @@ def test_adjustLSF():
     assert adjustedForm.oldValue == 'B12361006'
     assert adjustedForm.newValue == 'B12365892'
 
-@pytest.mark.integration
-def createOverloadForm():
-    # Honestly I am very lost here
-    # I don't know why this test is not even called when I run monitor.sh
-    k = 'weeklyHours'
-    createOverloadForm(rsp, k ,LSF, currentUser, adjustedForm, formHistories)
+    k = 'position'
+    adjustLSF(rsp, k, LSF, currentUser)
     adjustedForm = AdjustedForm.get(AdjustedForm.fieldAdjusted == k)
-    formHistories = FormHistory.get(FormHistory.historyType)
-    assert adjustedForm.oldValue == 20
-    assert adjustedForm.newValue == 10
-    assert formHistories == 'Labor Adjustment Form'
+    assert adjustedForm.oldValue == 'S61419'
+    assert adjustedForm.newValue == 'S61407'
+
+    k = 'weeklyHours'
+    adjustLSF(rsp, k, LSF, currentUser)
+    adjustedForm = AdjustedForm.get(AdjustedForm.fieldAdjusted == k)
+    assert adjustedForm.oldValue == '12'
+    assert adjustedForm.newValue == '10'
+
+
+# @pytest.mark.integration
+# def createOverloadForm():
+#     k = 'weeklyHours'
+#     createOverloadForm(rsp, k ,LSF, currentUser, adjustedForm, formHistories)
+#     adjustedForm = AdjustedForm.get(AdjustedForm.fieldAdjusted == k)
+#     formHistories = FormHistory.get(FormHistory.historyType)
+#     assert adjustedForm.oldValue == 20
+#     assert adjustedForm.newValue == 10
+#     assert formHistories == 'Labor Adjustment Form'
