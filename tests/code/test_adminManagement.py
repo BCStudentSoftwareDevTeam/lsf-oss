deb646c3d4a8440e067cf5886d36f87fdec6772e
Hila Manalai <manalaih@berea.edu>
2020-07-22 10:25:46 -0400

    checking fields values before changing them

--- tests/code/test_adminManagement.py
+++ tests/code/test_adminManagement.py
@@ -8,14 +8,18 @@ def test_addAdmin():
     newAdmin = "pearcej"
     user = User.get(User.username == newAdmin)
 
+    # Before adding user as admin
+    assert user.isLaborAdmin != True
     # Test adding labor admin
     addAdmin(user, 'labor')
     assert user.isLaborAdmin
 
+    assert user.isFinancialAidAdmin != True
     # Test adding financial aid admin
     addAdmin(user, 'finAid')
     assert user.isFinancialAidAdmin
 
+    assert user.isSaasAdmin != True
     # Test adding saas admin
     addAdmin(user, 'saas')
     assert user.isSaasAdmin
@@ -25,14 +29,18 @@ def test_removeAdmin():
     oldAdmin = "pearcej"
     user = User.get(User.username == oldAdmin)
 
+    # Before removing user as admin
+    assert user.isLaborAdmin == True
     # Test removing labor admin
     removeAdmin(user, 'labor')
     assert not user.isLaborAdmin
 
+    assert user.isFinancialAidAdmin == True
     # Test removing financial aid admin
     removeAdmin(user, 'finAid')
     assert not user.isFinancialAidAdmin
 
+    assert user.isSaasAdmin == True
     # Test removing saas admin
     removeAdmin(user, 'saas')
     assert not user.isSaasAdmin
