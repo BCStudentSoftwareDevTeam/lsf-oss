963ef9413e5f5cb92b1298defe0b681ec305af0c
Hila Manalai <manalaih@berea.edu>
2020-07-23 16:05:22 -0400

    removed print statements

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_nav.elements import *
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
 # login = LoginManager(app)  #FIXME: needs configured with our dev/prod environment handlers
 
 def load_config(file):
