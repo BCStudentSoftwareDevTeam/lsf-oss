ae2eb925ac2103ab81b588942674624e200c406d
Sreynit Khatt <sreynitkhatt@macbook-pro.home>
2020-07-24 14:54:27 -0400

    working on sidebar

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -6,9 +6,9 @@
 })
 */
 
-// $(document).ready(function(){
-//   $('.highlight').click(function(){
-//     $('a').removeClass("active");
-//     $(this).addClass("active");
-//   })
-// })
+$(document).ready(function(){
+  $('.highlight').click(function(){
+    $('.highlight').removeClass("activeMenuItem");
+    $(this).addClass("activeMenuItem");
+  });
+});
