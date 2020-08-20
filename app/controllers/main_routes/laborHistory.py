7a4853e415e15c8eaacd93863249b782681f899f
Brian Ramsay <brian@foont.net>
2020-08-20 14:17:16 -0400

    Sort the labor history properly

--- app/controllers/main_routes/laborHistory.py
+++ app/controllers/main_routes/laborHistory.py
@@ -31,7 +31,7 @@ def laborhistory(id):
             return render_template('errors/403.html'), 403
         student = Student.get(Student.ID == id)
         studentForms = LaborStatusForm.select().where(LaborStatusForm.studentSupervisee == student).order_by(LaborStatusForm.startDate.desc())
-        authorizedForms = studentForms
+        authorizedForms = set(studentForms)
         if not currentUser.isLaborAdmin:
             # View only your own form history
             if currentUser.student and not currentUser.supervisor:
@@ -40,11 +40,12 @@ def laborhistory(id):
             elif currentUser.supervisor and not currentUser.student:
                 supervisorForms = LaborStatusForm.select() \
                                   .join_from(LaborStatusForm, FormHistory) \
-                                  .where((LaborStatusForm.Supervisor == currentUser.supervisor.ID) | (FormHistory.createdBy == currentUser)) \
+                                  .where((LaborStatusForm.supervisor == currentUser.supervisor.ID) | (FormHistory.createdBy == currentUser)) \
                                   .distinct()
                 authorizedForms = set(studentForms).intersection(set(supervisorForms))
                 if len(authorizedForms) == 0:
                     return render_template('errors/403.html'), 403
+        authorizedForms = sorted(authorizedForms,key=lambda f:f.startDate, reverse=True)
         laborStatusFormList = ','.join([str(form.laborStatusFormID) for form in studentForms])
         return render_template( 'main/formHistory.html',
     				            title=('Labor History'),
