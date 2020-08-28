65ccc413ab22a5c24ba13bcfa5ac3028547955f1
Liberty Mupotsa <mupotsal@berea.edu>
2020-08-28 08:19:34 -0400

    working on the highlighht

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -2,18 +2,35 @@
 $(document).ready(function(){
   console.log("debug001");
   // $('.highlight').click(function(){
-  //   $('a').removeClass("active");
+  //   $('.navbar a').removeClass("active");
   //   $(this).addClass("active");
-  // })
+  // });
   let userAgentString = navigator.userAgent;
   // Detect Chrome
 
   let chromeAgent = userAgentString.indexOf("Chrome") > -1;
   console.log("Line21");
   console.log(chromeAgent);
-  if (chromeAgent == true){
-    $('a').removeAttr('tabindex');
-    $('li').removeAttr('tabindex');
-    $('div').removeAttr('tabindex');
+  var num = true;
+  if (num == true){
+    $('.navbar a').removeAttr('tabindex');
+    $('.navbar li').removeAttr('tabindex');
+    $('.navbar div').removeAttr('tabindex');
   }
 })
+
+// Get all buttons with class="btn" inside the container
+var all = document.getElementById("sidebar");
+var links = all.getElementsByClassName("highlight");
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    console.log("Trying to get the colors work 0000");
+    if (current != null){
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+    console.log("Trying to get the colors work")
+  }
+  });
+}
