d685923cf5ed48de737c6582665984083a1ebc11
ovezovs <ovezovs@ssdt-2020-ovezovs.ssdt.berea.edu>
2020-12-23 15:01:00 -0500

    completed the query and all logic in the controller

--- app/controllers/admin_routes/generalSearch.py
+++ app/controllers/admin_routes/generalSearch.py
@@ -1,21 +1,60 @@
 from app.controllers.admin_routes import admin
 from app.login_manager import require_login
-from flask import render_template
+from flask import render_template, request, json, jsonify
 from app.models.term import Term
 from app.models.department import Department
 from app.models.supervisor import Supervisor
 from app.models.student import Student
 from app.models.laborStatusForm import LaborStatusForm
 from app.models.formHistory import FormHistory
+from app.models.historyType import HistoryType
+from app.models.status import Status
 import operator
+from functools import reduce
 # from app.controllers.admin_routes.allPendingForms import x, y, z
 # from app.controllers.main_routes.laborHistory import x, y, z
 
-@admin.route('/admin/generalSearch', methods=['GET'])
+@admin.route('/admin/generalSearch', methods=['GET', 'POST'])
 def generalSearch():
+
     currentUser = require_login()
     if not currentUser or not currentUser.isLaborAdmin:
         return render_template('errors/403.html'), 403
+
+    query = None  # TODO: write if statements in JavaScript to handle the NoneType case
+    if request.method == 'POST':
+        queryResult = (request.data).decode("utf-8")  # This turns byte data into a string
+        queryDict = json.loads(queryResult)
+
+        fieldValueMap = { Term.termCode: queryDict['termCode'],
+                 Department.departmentID: queryDict['departmentID'],
+                 Student.ID: queryDict['studentID'],
+                 Supervisor.ID: queryDict['supervisorID'],
+                 FormHistory.historyType: queryDict['formType'],
+                 FormHistory.status: queryDict['formStatus'] }
+
+        clauses = []
+        for field, value in fieldValueMap.items():
+            if value:
+                # "is" is used to compare the two peewee objects.
+                if field is FormHistory.historyType:
+                    for val in value:
+                        clauses.append(field == val)
+                elif field is FormHistory.status:
+                    for val in value:
+                        clauses.append(field == val)
+                else:
+                    clauses.append(field == value)
+
+        expression = reduce(operator.and_, clauses)
+
+        query = (FormHistory.select().join(LaborStatusForm, on=(FormHistory.formID == LaborStatusForm.laborStatusFormID))
+                            .join(Department, on=(LaborStatusForm.department == Department.departmentID))
+                            .join(Supervisor, on=(LaborStatusForm.supervisor == Supervisor.ID))
+                            .join(Student, on=(LaborStatusForm.studentSupervisee == Student.ID))
+                            .join(Term, on=(LaborStatusForm.termCode == Term.termCode))
+                            .where(expression))
+        print("Almost there!!!")
 
     terms = Term.select()
     departments = Department.select()
@@ -27,43 +66,44 @@ def generalSearch():
                             terms = terms,
                             supervisors = supervisors,
                             students = students,
-                            departments = departments
+                            departments = departments,
+                            formList = query
                             )
 
-# TODO: A new function to run the search query
-@admin.route('/admin/generalSearch/<queryJson>', methods=['GET'])
-def generalSearchQuery(queryJson):
-    ''' query = {   term: Fall 2020,
-                    supervisor: Scott Heggen,
-                    student: Hila Manalai,
-                    department: CS,
-                    formType: {pending: False, approved: True, denied: False},
-                    formStatus: {adjusted: False, ...}
-
-    SELECT * FROM formhistory
-    JOIN laborstatusform ON laborstatusform.laborStatusFormID = formhistory.formID_id
-    JOIN department ON laborstatusform.department_id = department.departmentID
-    JOIN supervisor ON laborstatusform.supervisor_id = supervisor.ID
-    JOIN student ON laborstatusform.studentSupervisee_id = student.ID
-    JOIN term ON laborstatusform.termCode_id = term.termCode
-
-    WHERE department.departmentID = 0
-    AND supervisor.ID = 0
-    AND student.ID = 0
-    AND term.termCode = 201511;
-
-    '''
-
-    clauses = []
-    for key, value in queryJson.items():
-        if value:    # assuming value will be None if it is not selected by the user
-            clauses.append(key == value)
-
-    expression = reduce(operator.and_, clauses)
-
-    query = (FormHistory.select().join(LaborStatusForm on=(LaborStatusForm.laborStatusFormID == FormHistory.formID))
-                        .join(Department on=(LaborStatusForm.department == Department.departmentID))
-                        .join(Supervisor on=(LaborStatusForm.supervisor == Supervisor.ID))
-                        .join(Student on=(LaborStatusForm.student == Student.ID))
-                        .join(Term on=(LaborStatusForm.termCode == Term.termCode))
-                        .where(expression)
+# # TODO: A new function to run the search query
+# @admin.route('/admin/generalSearch/queryResult', methods=['POST'])
+# def generalSearchQuery():
+#     queryResult = (request.data).decode("utf-8")  # This turns byte data into a string
+#     queryDict = json.loads(queryResult)
+#
+#     fieldValueMap = { Term.termCode: queryDict['termCode'],
+#              Department.departmentID: queryDict['departmentID'],
+#              Student.ID: queryDict['studentID'],
+#              Supervisor.ID: queryDict['supervisorID'],
+#              FormHistory.historyType: queryDict['formType'],
+#              FormHistory.status: queryDict['formStatus'] }
+#
+#     clauses = []
+#     for field, value in fieldValueMap.items():
+#         if value:
+#             # "is" is used to compare the two peewee objects.
+#             if field is FormHistory.historyType:
+#                 for val in value:
+#                     clauses.append(field == val)
+#             elif field is FormHistory.status:
+#                 for val in value:
+#                     clauses.append(field == val)
+#             else:
+#                 clauses.append(field == value)
+#
+#     expression = reduce(operator.and_, clauses)
+#
+#     query = (FormHistory.select().join(LaborStatusForm, on=(FormHistory.formID == LaborStatusForm.laborStatusFormID))
+#                         .join(Department, on=(LaborStatusForm.department == Department.departmentID))
+#                         .join(Supervisor, on=(LaborStatusForm.supervisor == Supervisor.ID))
+#                         .join(Student, on=(LaborStatusForm.studentSupervisee == Student.ID))
+#                         .join(Term, on=(LaborStatusForm.termCode == Term.termCode))
+#                         .where(expression))
+#     print("Almost there!!!")
+#     return render_template('admin/allPendingForms.html',
+#                             formList=query)
---
+++ app/static/js/generalSearch.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  console.log("HELOOOOOOOO");
+});
+
+$("#generalSearchButton").on('click', function() {
+  var termCode = $("#termSelect").val();
+  var departmentID = $("#departmentSelect").val();
+  var supervisorID = $("#supervisorSelect").val();
+  var studentID = $("#studentSelect").val();
+  var formStatusList = [];
+  var formTypeList = [];
+
+	$("input:checkbox[name='formStatus']:checked").each(function(){
+      formStatusList.push($(this).val());
+	});
+
+  $("input:checkbox[name='formType']:checked").each(function(){
+      formTypeList.push($(this).val());
+	});
+
+  queryDict = {'termCode': termCode,
+               'departmentID': departmentID,
+               'supervisorID': supervisorID,
+               'studentID': studentID,
+               'formStatus': formStatusList,
+               'formType': formTypeList
+             };
+
+  var data = JSON.stringify(queryDict);
+  var url  = "/admin/generalSearch"
+  $.ajax({
+    method: "POST",
+    url: url,
+    data: data,
+    contentType: "application/json",
+    success: function(response) {
+      console.log("Success");
+    }
+  });
+});
