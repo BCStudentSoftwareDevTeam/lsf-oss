d714c92c9e38ed186aa794bcf905a185d0e628f2
Scott Heggen <heggens@berea.edu>
2021-09-24 13:37:11 -0400

    Install script created; reset_database.sh modified to still work

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-source db_install.sh
 source setup.sh
 
+
+if [ -n "${rootpasswd+x}" ]; then 
+        echo "There is a MySQL root password stored in the environment"; 
+else 
+        echo "Please enter root user MySQL password (Note: password will be hidden when typing):"
+        read -s rootpasswd
+fi
+
+source db_install.sh
+
+cd database
+source reset_database.sh
+cd ..
+
+unset rootpasswd
